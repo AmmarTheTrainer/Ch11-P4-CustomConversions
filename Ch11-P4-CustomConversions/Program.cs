bc8c73e408af4bbc105704e62c424b1e17a3e0fa
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-11 01:59:40 +0500

    Code Update 1

--- Ch11-P4-CustomConversions/Program.cs
+++ Ch11-P4-CustomConversions/Program.cs
@@ -10,6 +10,28 @@ namespace Ch11_P4_CustomConversions
     {
         static void Main(string[] args)
         {
+            #region Recall: Numerical Conversions
+
+            //int a = 123;
+            //long b = a; // Implicit conversion from int to long.
+            //int c = (int)b; // Explicit conversion from long to int.
+
+            #endregion
+
+            #region Conversions Among Related Class Types
+
+            //// Implicit cast between derived to base.
+            //Base myBaseType;
+            //myBaseType = new Derived();
+            //// Must explicitly cast to store base reference
+            //// in derived type.
+            //Derived myDerivedType = (Derived)myBaseType;
+
+            #endregion
+
+
+
+            Console.ReadLine();
 
         }
     }
